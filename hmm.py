178a7a340ed88d879487e18d340c5f868195b2b3
Malavika Venkatesan <malavika1996@hotmail.com>
2017-02-21 23:55:39 -0800

    changed some little things

--- hmm.py
+++ hmm.py
@@ -8,19 +8,19 @@ lines, word_map, num_map = ut.import_shakespeare(linear=True)
 lines = np.reshape(lines,(-1, 1))[:NUM_WORDS]
 states = word_map.keys()
 n_states = len(states)
+
 print 'getting the model'
 model = MultinomialHMM(n_components=NUM_WORDS)
 print 'fitting'
 model.fit(lines)
 print 'yay'
-hidden_states = model.predict(lines)
 for _ in range(14):
-	seed =np.random.randint(0, NUM_WORDS)
-	temp = model.sample(n_samples=8,random_state=seed)[1]
-	for i in range(len(temp)):
-		state = temp[i]
-		word = num_map[state]
-		if i == 0:
-			word = word.title()
-		sys.stdout.write(word + ' ')
-	print ''
+    seed = np.random.randint(0, NUM_WORDS)
+    model.random_state = seed
+    samples = model.sample(n_samples=8)[1]
+    for state in samples:
+        word = num_map[state]
+        if state == samples[0]:
+            word = word.capitalize()
+        sys.stdout.write(word + ' ')
+    print

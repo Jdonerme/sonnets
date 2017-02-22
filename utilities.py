5ecc6e041ed159b5df4ce0b8f1e86ca45f4750db
Jdonerme <jdonerme@caltech.edu>
2017-02-21 20:41:10 -0800

    updated import so that words are mapped to numbers

--- utilities.py
+++ utilities.py
@@ -1,6 +1,20 @@
+import sys
 def import_shakespeare(file="shakespeare.txt"):
+	''' imports a txt file in the format given of the shakespeare files.
+
+		Imports the file by constructing a 2D array where each element in
+		the array contains the lines of a sonnet.
+		Each line contains a list of numbers with each number mapping to 
+		a specific word. Words with punctuation are kept together for now.
+		Returns the sonnets array, word_map, and num_map.
+		(word_map maps words to numbers and num_maps vice versa)
+
+		'''
 	sonnets = []
 	lines = []
+	word_counter = 0
+	word_map = {}
+	num_map = {}
 	with open(file) as f:
 		for line in f:
 			line_split = line.strip('\n').split(" ")
@@ -8,10 +22,23 @@ def import_shakespeare(file="shakespeare.txt"):
 				line_split = filter(lambda a: a != '', line_split)
 				
 			if len(line_split) > 1:
-				lines.append(line_split)
+				coded_line = []
+				for word in line_split:
+					if word in word_map.keys():
+						coded_line.append(word_map[word])
+					else:
+						coded_line.append(word_counter)
+						word_map[word] = word_counter
+						num_map[word_counter] = word
+						word_counter += 1
+				lines.append(coded_line)
 				if len(lines) == 14:
 					sonnets.append(lines)
 					lines = []
-	print sonnets[17]
-import_shakespeare()
+	return sonnets, word_map, num_map
+'''s, w, n= import_shakespeare()
+for line in s[8]:
+	for word in line:
+		sys.stdout.write(str(n[word]) + " ")
+	print '\n' '''
 
